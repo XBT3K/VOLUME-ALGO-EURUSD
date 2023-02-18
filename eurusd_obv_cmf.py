177e9afeab6a124304944d2c3477eea2a91f040f
XBT3K <43539643+XBT3K@users.noreply.github.com>
2023-02-18 13:06:22 +0000

    Update eurusd_obv_cmf.py

--- eurusd_obv_cmf.py
+++ eurusd_obv_cmf.py
@@ -1,77 +1,72 @@
+import json
 import oandapyV20
-import oandapyV20.endpoints.orders as orders
+import oandapyV20.endpoints.instruments as instruments
 import pandas as pd
 
-def obv(df):
-    obv_values = []
-    obv = 0
-    for index, row in df.iterrows():
-        if row['Close'] > row['Close'].shift(1):
-            obv += row['Volume']
-        elif row['Close'] < row['Close'].shift(1):
-            obv -= row['Volume']
-        obv_values.append(obv)
-    return pd.Series(obv_values, index=df.index)
+# Load configuration from file
+with open('config.json') as f:
+    config = json.load(f)
 
-def cmf(df, period=20):
-    mfv = ((df['Close'] - df['Low']) - (df['High'] - df['Close'])) / (df['High'] - df['Low'])
-    mfv *= df['Volume']
-    cmf = mfv.rolling(period).sum() / df['Volume'].rolling(period).sum()
-    return cmf
+# Oanda API access credentials
+access_token = config['access_token']
+accountID = config['accountID']
 
-# Initialize the Oanda API client
-access_token = "YOUR_ACCESS_TOKEN"
-accountID = "YOUR_ACCOUNT_ID"
+# Trading parameters
+pair = config['pair']
+pos_size = config['pos_size']
+take_profit = config['take_profit']
+stop_loss = config['stop_loss']
+
+# Connect to Oanda API
 client = oandapyV20.API(access_token=access_token)
 
-# Define trading parameters
-pair = "EUR_USD"
-units = 10000
-take_profit = 0.0010
-stop_loss = 0.0010
-
-# Get historical candlestick data from Oanda
-params = {"count": 100, "granularity": "M15"}
+# Get latest 100 15-minute candlesticks of EUR/USD data
+params = {
+    'count': 100,
+    'granularity': 'M15'
+}
 r = instruments.InstrumentsCandles(instrument=pair, params=params)
-response = client.request(r)
-candles = response.get("candles")
-df = pd.DataFrame([(candle["time"], candle["mid"]["o"], candle["mid"]["h"], candle["mid"]["l"], candle["mid"]["c"], candle["volume"]) for candle in candles])
-df.columns = ["Time", "Open", "High", "Low", "Close", "Volume"]
-df["Time"] = pd.to_datetime(df["Time"])
-df = df.set_index("Time")
+data = client.request(r)
 
-# Calculate the OBV and CMF values
-df["OBV"] = obv(df)
-df["CMF"] = cmf(df)
+# Convert data to Pandas DataFrame
+df = pd.DataFrame([[candle['time'], candle['mid']['o'], candle['mid']['h'], candle['mid']['l'], candle['mid']['c'], candle['volume']] for candle in data['candles']])
+df.columns = ['time', 'open', 'high', 'low', 'close', 'volume']
+df['time'] = pd.to_datetime(df['time'])
+df = df.set_index('time')
 
-# Define the trading strategy based on the OBV and CMF values
-if df["OBV"].iloc[-1] > df["OBV"].iloc[-2] and df["CMF"].iloc[-1] > df["CMF"].iloc[-2]:
-    order_type = "buy"
-elif df["OBV"].iloc[-1] < df["OBV"].iloc[-2] and df["CMF"].iloc[-1] < df["CMF"].iloc[-2]:
-    order_type = "sell"
+# Calculate OBV and CMF indicators
+df['obv'] = df['volume'].cumsum()
+df['cmf'] = ((df['close'] - df['low']) - (df['high'] - df['close'])) / (df['high'] - df['low']) * df['volume']
+df['cmf'] = df['cmf'].rolling(20).sum() / df['volume'].rolling(20).sum()
+
+# Execute trade based on OBV and CMF signals
+if df['obv'].iloc[-1] > df['obv'].iloc[-2] and df['cmf'].iloc[-1] > 0:
+    units = pos_size
+    side = 'buy'
+elif df['obv'].iloc[-1] < df['obv'].iloc[-2] and df['cmf'].iloc[-1] < 0:
+    units = -pos_size
+    side = 'sell'
 else:
-    order_type = "hold"
+    units = 0
+    side = 'hold'
 
-# Execute the trade if the trading strategy signals a buy or sell order
-if order_type == "buy":
-    data = {
-        "order": {
-            "instrument": pair,
-            "units": units,
-            "type": "MARKET",
-            "positionFill": "DEFAULT",
-            "takeProfitOnFill": {
-                "price": df["Close"].iloc[-1] + take_profit
+if units != 0:
+    # Create market order
+    order_data = {
+        'order': {
+            'units': str(units),
+            'instrument': pair,
+            'timeInForce': 'FOK',
+            'type': 'MARKET',
+            'positionFill': 'DEFAULT',
+            'side': side,
+            'takeProfitOnFill': {
+                'price': str(df['close'].iloc[-1] + units * take_profit)
             },
-            "stopLossOnFill": {
-                "price": df["Close"].iloc[-1] - stop_loss
+            'stopLossOnFill': {
+                'price': str(df['close'].iloc[-1] - units * stop_loss)
             }
         }
     }
-    r = orders.OrderCreate(accountID, data=data)
-    response = client.request(r)
-    print(response)
-elif order_type == "sell":
-    data = {
-        "order": {
-            "instrument": pair
+    r = oandapyV20.endpoints.orders.OrderCreate(accountID, data=order_data)
+    client.request(r)
